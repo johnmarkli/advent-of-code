857812dcb255b5af8d8ebbc1d4bcfb5a6f63900d
John Li <johnmarkli@gmail.com>
2022-12-09 13:30:47 -0500

    Day 3

--- 2022/main.go
+++ 2022/main.go
@@ -7,4 +7,6 @@ func main() {
 	fmt.Println("Day 1 - Part 2:", Day1Part2("testdata/day1"))
 	fmt.Println("Day 2 - Part 1:", Day2Part1("testdata/day2"))
 	fmt.Println("Day 2 - Part 2:", Day2Part2("testdata/day2"))
+	fmt.Println("Day 3 - Part 1:", Day3Part1("testdata/day3"))
+	fmt.Println("Day 3 - Part 2:", Day3Part2("testdata/day3"))
 }
--- 2022/main_test.go
+++ 2022/main_test.go
@@ -15,3 +15,8 @@ func TestDay2(t *testing.T) {
 	require.Equal(t, 15, Day2Part1("testdata/day2_example"))
 	require.Equal(t, 12, Day2Part2("testdata/day2_example"))
 }
+
+func TestDay3(t *testing.T) {
+	require.Equal(t, 157, Day3Part1("testdata/day3_example"))
+	require.Equal(t, 70, Day3Part2("testdata/day3_example"))
+}
