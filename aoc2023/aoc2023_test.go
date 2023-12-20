73695933cc97b88de7e5e47135e0ddf4a92ad93d
John Li <johnmarkli@gmail.com>
2023-12-20 10:22:52 -0500

    Clean up and ignore puzzle inputs

--- aoc2023/aoc2023_test.go
+++ aoc2023/aoc2023_test.go
@@ -9,79 +9,79 @@ import (
 )
 
 func TestDay1(t *testing.T) {
-	require.Equal(t, 142, aoc2023.Day1Part1("testdata/day1_example"))
+	require.Equal(t, 142, aoc2023.Day1Part1("testdata/day1"))
 
-	require.Equal(t, 281, aoc2023.Day1Part2("testdata/day1_example2"))
+	require.Equal(t, 281, aoc2023.Day1Part2("testdata/day1-2"))
 }
 
 func TestDay2(t *testing.T) {
-	require.Equal(t, 8, aoc2023.Day2Part1("testdata/day2_example"))
+	require.Equal(t, 8, aoc2023.Day2Part1("testdata/day2"))
 
-	require.Equal(t, 2286, aoc2023.Day2Part2("testdata/day2_example"))
+	require.Equal(t, 2286, aoc2023.Day2Part2("testdata/day2"))
 }
 
 func TestDay3(t *testing.T) {
-	require.Equal(t, 4361, aoc2023.Day3Part1("testdata/day3_example"))
-	require.Equal(t, 4361, aoc2023.Day3Part1("testdata/day3_example2"))
+	require.Equal(t, 4361, aoc2023.Day3Part1("testdata/day3"))
+	require.Equal(t, 4361, aoc2023.Day3Part1("testdata/day3-2"))
 
-	require.Equal(t, 467835, aoc2023.Day3Part2("testdata/day3_example"))
-	require.Equal(t, 467835, aoc2023.Day3Part2("testdata/day3_example2"))
-	require.Equal(t, 467835, aoc2023.Day3Part2("testdata/day3_example3"))
+	require.Equal(t, 467835, aoc2023.Day3Part2("testdata/day3"))
+	require.Equal(t, 467835, aoc2023.Day3Part2("testdata/day3-2"))
+	require.Equal(t, 467835, aoc2023.Day3Part2("testdata/day3-3"))
 }
 
 func TestDay4(t *testing.T) {
-	require.Equal(t, 13, aoc2023.Day4Part1("testdata/day4_example"))
+	require.Equal(t, 13, aoc2023.Day4Part1("testdata/day4"))
 
-	require.Equal(t, 30, aoc2023.Day4Part2("testdata/day4_example"))
+	require.Equal(t, 30, aoc2023.Day4Part2("testdata/day4"))
 }
 
 func TestDay5(t *testing.T) {
-	require.Equal(t, 35, aoc2023.Day5Part1("testdata/day5_example"))
+	require.Equal(t, 35, aoc2023.Day5Part1("testdata/day5"))
 
-	require.Equal(t, 46, aoc2023.Day5Part2("testdata/day5_example"))
+	require.Equal(t, 46, aoc2023.Day5Part2("testdata/day5"))
 }
 
 func TestDay6(t *testing.T) {
-	require.Equal(t, 288, aoc2023.Day6Part1("testdata/day6_example"))
+	require.Equal(t, 288, aoc2023.Day6Part1("testdata/day6"))
 
-	require.Equal(t, 71503, aoc2023.Day6Part2("testdata/day6_example"))
+	require.Equal(t, 71503, aoc2023.Day6Part2("testdata/day6"))
 }
 
 func TestDay7(t *testing.T) {
-	require.Equal(t, 6440, aoc2023.Day7Part1("testdata/day7_example"))
+	require.Equal(t, 6440, aoc2023.Day7Part1("testdata/day7"))
 
-	require.Equal(t, 5905, aoc2023.Day7Part2("testdata/day7_example"))
+	require.Equal(t, 5905, aoc2023.Day7Part2("testdata/day7"))
 }
 
 func TestDay8(t *testing.T) {
-	require.Equal(t, 2, aoc2023.Day8Part1("testdata/day8_example"))
-	require.Equal(t, 6, aoc2023.Day8Part1("testdata/day8_example2"))
+	require.Equal(t, 2, aoc2023.Day8Part1("testdata/day8"))
+	require.Equal(t, 6, aoc2023.Day8Part1("testdata/day8-2"))
 
-	require.Equal(t, 6, aoc2023.Day8Part2("testdata/day8_example3"))
+	require.Equal(t, 6, aoc2023.Day8Part2("testdata/day8-3"))
 }
 
 func TestDay9(t *testing.T) {
-	require.Equal(t, 114, aoc2023.Day9Part1("testdata/day9_example"))
-	require.Equal(t, 2, aoc2023.Day9Part2("testdata/day9_example"))
+	require.Equal(t, 114, aoc2023.Day9Part1("testdata/day9"))
+	require.Equal(t, 2, aoc2023.Day9Part2("testdata/day9"))
 }
 
 func TestDay10(t *testing.T) {
-	require.Equal(t, 4, aoc2023.Day10Part1("testdata/day10_example"))
-	require.Equal(t, 4, aoc2023.Day10Part1("testdata/day10_example2"))
-	require.Equal(t, 8, aoc2023.Day10Part1("testdata/day10_example3"))
-	require.Equal(t, 8, aoc2023.Day10Part1("testdata/day10_example4"))
+	require.Equal(t, 4, aoc2023.Day10Part1("testdata/day10"))
+	require.Equal(t, 4, aoc2023.Day10Part1("testdata/day10-2"))
+	require.Equal(t, 8, aoc2023.Day10Part1("testdata/day10-3"))
+	require.Equal(t, 8, aoc2023.Day10Part1("testdata/day10-4"))
 
-	require.Equal(t, 4, aoc2023.Day10Part2("testdata/day10_example5"))
-	require.Equal(t, 8, aoc2023.Day10Part2("testdata/day10_example6"))
-	require.Equal(t, 10, aoc2023.Day10Part2("testdata/day10_example7"))
+	require.Equal(t, 4, aoc2023.Day10Part2("testdata/day10-5"))
+	require.Equal(t, 8, aoc2023.Day10Part2("testdata/day10-6"))
+	require.Equal(t, 10, aoc2023.Day10Part2("testdata/day10-7"))
 }
 
 func TestDay11(t *testing.T) {
-	require.Equal(t, 374, aoc2023.Day11Part1("testdata/day11_example"))
-	require.Equal(t, 7, aoc2023.Day11Part1("testdata/day11_example2"))
+	require.Equal(t, 374, aoc2023.Day11Part1("testdata/day11"))
+	require.Equal(t, 7, aoc2023.Day11Part1("testdata/day11-2"))
 
-	require.Equal(t, 1030, aoc2023.Day11Part2Solve("testdata/day11_example", 10))
-	require.Equal(t, 8410, aoc2023.Day11Part2Solve("testdata/day11_example", 100))
+	require.Equal(t, 1030, aoc2023.Day11Part2Solve("testdata/day11", 10))
+	require.Equal(t, 8410, aoc2023.Day11Part2Solve("testdata/day11", 100))
 }
 
 func TestDay12(t *testing.T) {
@@ -137,7 +137,13 @@ func TestDay12(t *testing.T) {
 		require.Equal(t, tc.expect, tc.sr.Arrangements())
 	}
 
-	require.Equal(t, 21, aoc2023.Day12Part1("testdata/day12_example"))
+	require.Equal(t, 21, aoc2023.Day12Part1("testdata/day12"))
 
-	require.Equal(t, 525152, aoc2023.Day12Part2("testdata/day12_example"))
+	require.Equal(t, 525152, aoc2023.Day12Part2("testdata/day12"))
+}
+
+func TestDay13(t *testing.T) {
+	require.Equal(t, 0, aoc2023.Day13Part1("testdata/day13"))
+
+	require.Equal(t, 0, aoc2023.Day13Part2("testdata/day13-2"))
 }
---
+++ aoc2023/day13.go
@@ -0,0 +1,21 @@
+package aoc2023
+
+// Day13Part1 ...
+func Day13Part1(filepath string) any {
+	var result int
+
+	// file, scanner := readFile(filepath)
+	// defer file.Close()
+
+	return result
+}
+
+// Day13Part2 ...
+func Day13Part2(filepath string) any {
+	var result int
+
+	// file, scanner := readFile(filepath)
+	// defer file.Close()
+
+	return result
+}
